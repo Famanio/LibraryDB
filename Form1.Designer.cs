b0321cbf11f225ebd713666c13808e520fbbb8c1
Famanio <jascharov.jaroslaw.62@gmail.com>
2022-12-05 17:13:32 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace LibraryDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.selectTableCB = new System.Windows.Forms.ComboBox();
+            this.selectButton = new System.Windows.Forms.Button();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.mainDGV = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mainDGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // selectTableCB
+            // 
+            this.selectTableCB.FormattingEnabled = true;
+            this.selectTableCB.Location = new System.Drawing.Point(3, 3);
+            this.selectTableCB.Name = "selectTableCB";
+            this.selectTableCB.Size = new System.Drawing.Size(196, 28);
+            this.selectTableCB.TabIndex = 0;
+            // 
+            // selectButton
+            // 
+            this.selectButton.Location = new System.Drawing.Point(205, 3);
+            this.selectButton.Name = "selectButton";
+            this.selectButton.Size = new System.Drawing.Size(94, 29);
+            this.selectButton.TabIndex = 1;
+            this.selectButton.Text = "Загрузить";
+            this.selectButton.UseVisualStyleBackColor = true;
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.searchTextBox.Location = new System.Drawing.Point(720, 3);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.PlaceholderText = "Поиск";
+            this.searchTextBox.Size = new System.Drawing.Size(180, 27);
+            this.searchTextBox.TabIndex = 2;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(3, 36);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(897, 498);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.mainDGV);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(889, 465);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Таблица";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // mainDGV
+            // 
+            this.mainDGV.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainDGV.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.mainDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.mainDGV.Location = new System.Drawing.Point(6, 6);
+            this.mainDGV.Name = "mainDGV";
+            this.mainDGV.RowHeadersWidth = 51;
+            this.mainDGV.RowTemplate.Height = 29;
+            this.mainDGV.Size = new System.Drawing.Size(877, 453);
+            this.mainDGV.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(889, 465);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Редактирование";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 29);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(889, 465);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Запросы";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(903, 546);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.selectButton);
+            this.Controls.Add(this.selectTableCB);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "База данных библиотеки";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.mainDGV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ComboBox selectTableCB;
+        private Button selectButton;
+        private TextBox searchTextBox;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private TabPage tabPage3;
+        private DataGridView mainDGV;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,12 @@
+
+
+namespace LibraryDB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
